293079161131e4a80307a70bf490bd23f56b3af2
Michael Buzzard <m.buzzard87@gmail.com>
2021-10-25 19:16:15 -0400

    add animal/dog class

---
+++ VirtualOverrideDemo/Dog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOverrideDemo
+{
+    class Dog : Animal
+    {
+
+        public bool IsHappy { get; set; }
+
+        public Dog(string name, int age) : base(name,age)
+        {
+            IsHappy = true;
+        }
+
+        //Override virtual Eat method within the Animal class
+        public override void Eat()
+        {
+
+            // To call the Eat method from the base class (Animal) we use the keyword "base"
+            base.Eat();
+        }
+
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Woof!!!");
+        }
+
+        public override void Play()
+        {
+            if(IsHappy)
+            {
+                base.Play();
+            }
+        }
+
+    }
+}
---
+++ VirtualOverrideDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOverrideDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dog dog = new Dog("Shia", 10);
+            Console.WriteLine($"{dog.Name} is {dog.Age} years old");
+            dog.Play();
+            dog.Eat();
+            dog.MakeSound();
+
+            Console.ReadKey();
+        }
+    }
+}
