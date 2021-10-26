0415ab8392291d3f7e3fdcffc2054d45b0f3910d
Michael Buzzard <m.buzzard87@gmail.com>
2021-10-26 19:43:29 -0400

    add car class

---
+++ VirtualOverrideDemo/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOverrideDemo
+{
+    class Car : Vehicle
+    {
+
+        public Car(string color, int topSpeed)
+        {
+            Color = color;
+            TopSpeed = topSpeed;
+        }
+    }
+}
