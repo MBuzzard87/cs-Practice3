9ef5ea39552052e7ddb78547cb6f2022de8739fe
Michael Buzzard <m.buzzard87@gmail.com>
2021-10-26 18:50:13 -0400

    add trainee class

---
+++ VirtualOverrideDemo/Trainee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOverrideDemo
+{
+    class Trainee : Employee
+    {
+
+        public int WorkingHours { get; set; }
+        public int SchoolHours { get; set; }
+
+        public void Learn() { }
+
+        public override void Work() { }
+
+    }
+}
