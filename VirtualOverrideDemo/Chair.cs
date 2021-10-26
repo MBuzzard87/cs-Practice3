d64cfdd1dbee01775ad1063ebb0fe5a80744c35e
Michael Buzzard <m.buzzard87@gmail.com>
2021-10-26 19:43:40 -0400

    add chair class

---
+++ VirtualOverrideDemo/Chair.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOverrideDemo
+{
+    class Chair : Furniture
+    {
+
+        public Chair(string color, string material)
+        {
+            Color = color;
+            Material = material;
+        }
+    }
+}
