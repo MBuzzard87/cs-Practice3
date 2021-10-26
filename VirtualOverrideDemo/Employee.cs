5009ae0af14a0e471440dc19a35b6da6adaf8196
Michael Buzzard <m.buzzard87@gmail.com>
2021-10-26 18:42:16 -0400

    add employee class

---
+++ VirtualOverrideDemo/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOverrideDemo
+{
+    class Employee
+    {
+
+        public string FirstName { get; set; }
+        public int Salary { get; set; }
+
+        public void Work()
+        {
+
+        }
+
+        public void Pause() { }
+
+    }
+}
