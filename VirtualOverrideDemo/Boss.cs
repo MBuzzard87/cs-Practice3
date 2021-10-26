3848f75e8a150c2937498cc248b928051bd2674b
Michael Buzzard <m.buzzard87@gmail.com>
2021-10-26 18:49:01 -0400

    add boss class

---
+++ VirtualOverrideDemo/Boss.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOverrideDemo
+{
+    class Boss : Employee
+    {
+
+        private bool companyCar;
+
+        public Boss(string firstName, int salary)
+        {
+            FirstName = firstName;
+            Salary = salary;
+            companyCar = true;
+        }
+
+
+    }
+}
